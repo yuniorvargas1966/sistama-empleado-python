f48eda21ea507c254a52322cbcef182e7c90b1d4
Yunior Vargas Hernández <yuniorvargas1966@gmail.com>
2023-02-20 22:45:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_HOST'] = 'containers-us-west-167.railway.app'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'vKYINRwWohPOUgyTTiy7'
-app.config['MYSQL_DATABASE_DB'] = 'railway'
-app.config['MYSQL_DATABASE_PORT'] = '7592'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'sistema'
 mysql.init_app(app)
 
 CARPETA= os.path.join('uploads')
